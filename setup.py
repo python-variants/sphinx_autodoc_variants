8c02851290aa195511fa0b58dd68531d1b82ecff
Paul Ganssle <paul@ganssle.io>
2018-05-03 19:02:32 -0400

    Update setup metadata
    Various metadata is not supported in setup.cfg or
    setup.cfg was misconfigured.

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import setuptools
+
+import io
+
+with io.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     setup_requires=['setuptools_scm'],
     use_scm_version={'write_to': 'src/sphinx_autodoc_variants/_version.py'},
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
 )
